64dd4f567fe1170bee0c327ddf87d97881c13a33
geniinho <geniinho@hotmail.com>
2022-05-12 10:09:04 -0300

    padrão command

---
+++ src/main/java/br/com/cod3r/command/alexa/commands/TurnLightOnCommand.java
@@ -0,0 +1,16 @@
+package br.com.cod3r.command.alexa.commands;
+
+import br.com.cod3r.command.alexa.lights.GenericLight;
+
+public class TurnLightOnCommand implements Command{
+    private GenericLight light;
+
+    public TurnLightOnCommand(GenericLight light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOn();
+    }
+}

74969461aec1af0443a67a8c091b6bb52f2dd44a
geniinho <geniinho@hotmail.com>
2022-05-12 17:07:23 -0300

    Padrão Observer

---
+++ src/main/java/br/com/cod3r/observer/party/Client.java
@@ -0,0 +1,16 @@
+package br.com.cod3r.observer.party;
+
+import br.com.cod3r.observer.party.observers.Friend;
+import br.com.cod3r.observer.party.observers.Wife;
+import br.com.cod3r.observer.party.subject.Doorman;
+
+public class Client {
+    public static void main(String[] args) {
+        Doorman doorman = new Doorman();
+        doorman.add(new Wife());
+        doorman.add(new Friend());
+
+        System.out.println("Husband is comming");
+        doorman.setStatus(true);
+    }
+}
